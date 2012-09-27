46103b2634ef6aa8a6defdb4a47e449b8fbe3bbd
Swayonic <swayonic@gmail.com>
2012-09-27 00:47:43 -0600

    Created initial rails project at mpd/

---
+++ mpd/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpd::Application.config.secret_token = '1875417887d36a1d960ab148b1fd36bca87358fab45350dcff9a5b81103a7dec1ef9fad62d6a89408a01944dfe1aa86b2a3cc8ae5095e2b71965baf8da60d69e'
