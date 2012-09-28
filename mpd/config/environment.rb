17473e2b819339020cc013876d86a2f778726bdd
Swayonic <swayonic@gmail.com>
2012-09-28 02:34:29 -0600

    Configured server to run with Passenger/Apache.
    Required minimal changes in this code, just added a welcome controller.

--- mpd/config/environment.rb
+++ mpd/config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Not necessary, gets set in /etc/apache2/httpd.conf
+#ENV['RAILS_ENV'] = 'development'
+
 # Initialize the rails application
 Mpd::Application.initialize!
+
