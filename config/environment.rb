4ae2bf7125727db77fd64c5be59f5d6f2b17ac4c
Swayonic <swayonic@gmail.com>
2012-12-13 00:34:13 -0700

    Not compiling javascripts into application.js anymore
    This is because the order of loading JS files matters with the raphael
    files. I figured out how to use the production asset pipeline in
    development, which finally gives me some confidence that this is right.

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Not necessary, gets set in /etc/apache2/httpd.conf
-#ENV['RAILS_ENV'] = 'development'
+ENV['RAILS_ENV'] = 'development'
 
 # Initialize the rails application
 Mpd::Application.initialize!
