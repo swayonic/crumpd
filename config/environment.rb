50b6e82f772a026ab86c8becd2d7340e26133b2b
Swayonic <swayonic@gmail.com>
2012-12-16 02:42:01 -0700

    Trying to get Heroku to run in production mode

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Not necessary, gets set in /etc/apache2/httpd.conf
-ENV['RAILS_ENV'] = 'development'
+#ENV['RAILS_ENV'] = 'development'
 
 # Initialize the rails application
 Mpd::Application.initialize!
