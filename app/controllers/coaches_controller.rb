411d926cf7378bbfa5893e106fb7394e7aede37f
Swayonic <swayonic@gmail.com>
2013-02-03 00:49:46 -0700

    Updated groups#show to allow adding of coaches by account number too

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -7,17 +7,63 @@ class CoachesController < HomeController
 			render 'shared/unauthorized'
 			return
 		end
-
-		@coach = @group.group_coaches.build(params[:group_coach])
-
-		if @coach.user.nil?
-			flash.alert = 'No user found'
-		elsif @group.coaches.select{ |u| u.id == @coach.user_id }.count > 0
-			flash.notice = 'Coach already exists'
-		elsif @coach.save
-			flash.notice = 'Coach added'
-		else
+		
+		if params[:add_coach].nil?
 			flash.alert = 'Coach not added'
+			redirect_to @group
+			return
+		end
+
+		# Adding a new user
+		if params[:add_coach][:new] == '1'
+			if params[:add_coach][:account_number] =~ /^\s*(\d+)\s*$/
+				if !user = User.find_by_account_number($1)
+					user = User.new
+					user.account_number = $1
+
+					# TODO: Verify with sitrack
+
+					user.save
+					redirect_to(
+						:controller => :users,
+						:action => :confirm,
+						:id => user.id,
+						:continue => url_for(
+							:action => :create,
+							'add_coach[new]' => '0',
+							'add_coach[user_id]' => user.id
+							)
+						)
+					return
+				else
+					user_id = user.id
+					# Fall through to existing user
+				end
+			else
+				flash.notice = 'Coach not added'
+				redirect_to @group
+				return
+			end
+
+		# Adding an existing user
+		else
+			user_id = $1 if params[:add_coach][:user_id] =~ /^(\d+)$/
+		end
+
+		if user_id.nil?
+			flash.alert = 'No member selected'
+		else
+			@coach = @group.group_coaches.build(:user_id => user_id)
+
+			if @coach.user.nil?
+				flash.alert = 'No user found'
+			elsif @group.coaches.select{ |u| u.id == @coach.user_id }.count > 0
+				flash.notice = 'Coach already exists'
+			elsif @coach.save
+				flash.notice = 'Coach added'
+			else
+				flash.alert = 'Coach not added'
+			end
 		end
 
 		redirect_to @group
