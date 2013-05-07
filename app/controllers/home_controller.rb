b961fb88afbda3ac9deaf8a8f9b7f8703198b523
Swayonic <swayonic@gmail.com>
2013-05-07 22:07:21 +0800

    Refactor tabs out of controllers (again)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,46 +1,46 @@
 class HomeController < ApplicationController
 
   # GET /
-	def index
-	end
+  def index
+  end
 
   # GET /login
-	def login
-	end
+  def login
+  end
 
   # POST /do_login
-	def do_login
-		if newuser = User.find_by_id(params[:login][:id])
-			session[:user_id] = newuser.id
-			redirect_to :action => :index
-		else
-			flash.now[:alert] = 'No user found'
-			render :login
-		end
-	end
+  def do_login
+    if newuser = User.find_by_id(params[:login][:id])
+      session[:user_id] = newuser.id
+      redirect_to :action => :index
+    else
+      flash.now[:alert] = 'No user found'
+      render :login
+    end
+  end
 
   # GET /logout
-	def logout
-		if Rails.env.production?
-			RubyCAS::Filter.logout(self)
-		else
-			session[:user_id] = nil
-			session[:sudo_id] = nil
-			redirect_to :action => :index
-		end
-	end
+  def logout
+    if Rails.env.production?
+      RubyCAS::Filter.logout(self)
+    else
+      session[:user_id] = nil
+      session[:sudo_id] = nil
+      redirect_to :action => :index
+    end
+  end
 
   # POST /full_update
-	def full_update
-		if !@cas_user.is_admin?
-			render 'shared/forbidden'
-			return
-		end
+  def full_update
+    if !@cas_user.is_admin?
+      render 'shared/forbidden'
+      return
+    end
 
-	  # Manually perform system-wide data update
-		Sitrack.update_all
-		redirect_to :back
-	end
+    # Manually perform system-wide data update
+    Sitrack.update_all
+    redirect_to :back
+  end
 
   # POST /view_user
   def view_user
